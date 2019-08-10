948d123ba069c584a627bf4f43365f904fbec3c5
David Sevilla Martin <dsevilla192@icloud.com>
2019-08-10 11:14:05 -0400

    Rework code to implement new exception handling rewrite features

---
+++ src/Reporters/SentryReporter.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace FoF\Sentry\Reporters;
+
+
+use Flarum\Foundation\ErrorHandling\HandledError;
+use Flarum\Foundation\ErrorHandling\Reporter;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Sentry\State\HubInterface;
+use Sentry\State\Scope;
+
+class SentryReporter implements Reporter
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function report(HandledError $error)
+    {
+        /**
+         * @var $hub HubInterface
+         * @var $request ServerRequestInterface
+         * @var $stack string
+         */
+        $hub = app('sentry');
+        $request = app('sentry.request');
+        $id = null;
+
+        if ($hub == null) {
+            app('log')->warn('[fof/sentry] sentry dsn not set');
+
+            return;
+        }
+
+        $hub->configureScope(function (Scope $scope) use ($request, $error) {
+            $scope->setExtra('details', $error->getDetails());
+
+            $user = $request->getAttribute('actor');
+
+            if ($user != null && $user->id != 0) {
+                $scope->setUser([
+                    'id'       => $user->id,
+                    'username' => $user->username,
+                    'email'    => $user->email,
+                ]);
+            }
+        });
+
+        $err = $error->getError();
+        $id = $hub->captureException($error->getError());
+
+        if ($id == null) {
+            $this->logger->warning('[fof/sentry] exception of type '.get_class($err).' failed to send');
+        }
+    }
+}
