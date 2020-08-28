ea13cf3749022e1f13476a96663d349e9e76f395
David Sevilla Martin <me@datitisev.me>
2020-08-28 11:30:54 -0400

    Update dependencies & update for beta 14

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -7,20 +7,22 @@ const {
 
 app.initializers.add('fof/sentry', () => {
     app.extensionSettings['fof-sentry'] = () =>
-        app.modal.show(
-            new SettingsModal({
-                title: 'FriendsOfFlarum Sentry',
-                type: 'small',
-                items: [
-                    <StringItem key="fof-sentry.dsn" type="url" required>
-                        {app.translator.trans('fof-sentry.admin.settings.dsn_label')}
-                    </StringItem>,
-                    <BooleanItem key="fof-sentry.user_feedback">{app.translator.trans('fof-sentry.admin.settings.user_feedback_label')}</BooleanItem>,
-                    <BooleanItem key="fof-sentry.javascript">{app.translator.trans('fof-sentry.admin.settings.javascript_label')}</BooleanItem>,
-                    <BooleanItem key="fof-sentry.javascript.console">
-                        {app.translator.trans('fof-sentry.admin.settings.javascript_console_label')}
-                    </BooleanItem>,
-                ],
-            })
-        );
+        app.modal.show(SettingsModal, {
+            title: 'FriendsOfFlarum Sentry',
+            type: 'small',
+            items: (s) => [
+                <StringItem name="fof-sentry.dsn" type="url" required setting={s}>
+                    {app.translator.trans('fof-sentry.admin.settings.dsn_label')}
+                </StringItem>,
+                <BooleanItem name="fof-sentry.user_feedback" setting={s}>
+                    {app.translator.trans('fof-sentry.admin.settings.user_feedback_label')}
+                </BooleanItem>,
+                <BooleanItem name="fof-sentry.javascript" setting={s}>
+                    {app.translator.trans('fof-sentry.admin.settings.javascript_label')}
+                </BooleanItem>,
+                <BooleanItem name="fof-sentry.javascript.console" setting={s}>
+                    {app.translator.trans('fof-sentry.admin.settings.javascript_console_label')}
+                </BooleanItem>,
+            ],
+        });
 });
