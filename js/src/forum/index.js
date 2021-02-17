2fce6d5542758126b7a78617e92ec46e19324ff8
David Wheatley <hi@davwheat.dev>
2021-02-17 16:40:12 +0000

    Refactor user identification JS

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,22 +7,23 @@ window.Sentry.Integrations.CaptureConsole = CaptureConsole;
 window.Sentry.TracingIntegrations = TracingIntegrations;
 
 window.Sentry.getUserData = (nameAttr = 'username') => {
+    /** @type {Sentry.User} */
+    let userData = {};
+
+    // Depending on when the error occurs, `app` might not be defined
     if (app) {
         if (app.session && app.session.user && app.session.user.id() != 0) {
-            const data = {
+            userData = {
                 id: app.session.user.id(),
                 email: app.session.user.email(),
+                [nameAttr]: app.session.user.username(),
             };
-
-            data[nameAttr] = app.session.user.username();
-
-            return data;
         } else if (app.data.session && app.data.session.userId != 0) {
-            return {
+            userData = {
                 id: app.data.session.userId,
             };
         }
     }
 
-    return {};
+    return userData;
 };
