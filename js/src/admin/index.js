da3d27d3d23c3a8ee296a6ccc71ecbeb2fdc9be1
David Wheatley <hi@davwheat.dev>
2021-02-17 16:23:44 +0000

    Add front-end performance sample rate setting
    Same 0-100 range as the back-end option. Converted to 0-1 decimal in PHP.

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,30 +1,36 @@
 import app from 'flarum/app';
 
 app.initializers.add('fof/sentry', () => {
-  app.extensionData.for('fof-sentry')
-    .registerSetting({
-      label: app.translator.trans('fof-sentry.admin.settings.dsn_label'),
-      setting: 'fof-sentry.dsn',
-      type: 'url',
-    })
-    .registerSetting({
-      label: app.translator.trans('fof-sentry.admin.settings.user_feedback_label'),
-      setting: 'fof-sentry.user_feedback',
-      type: 'boolean',
-    })
-    .registerSetting({
-      label: app.translator.trans('fof-sentry.admin.settings.javascript_label'),
-      setting: 'fof-sentry.javascript',
-      type: 'boolean',
-    })
-    .registerSetting({
-      label: app.translator.trans('fof-sentry.admin.settings.javascript_console_label'),
-      setting: 'fof-sentry.javascript.console',
-      type: 'boolean',
-    })
-    .registerSetting({
-      label: app.translator.trans('fof-sentry.admin.settings.monitor_performance_label'),
-      setting: 'fof-sentry.monitor_performance',
-      type: 'input',
-    })
+    app.extensionData
+        .for('fof-sentry')
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.dsn_label'),
+            setting: 'fof-sentry.dsn',
+            type: 'url',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.user_feedback_label'),
+            setting: 'fof-sentry.user_feedback',
+            type: 'boolean',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.monitor_performance_label'),
+            setting: 'fof-sentry.monitor_performance',
+            type: 'input',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.javascript_label'),
+            setting: 'fof-sentry.javascript',
+            type: 'boolean',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.javascript_console_label'),
+            setting: 'fof-sentry.javascript.console',
+            type: 'boolean',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-sentry.admin.settings.javascript_trace_sample_rate'),
+            setting: 'fof-sentry.javascript.trace_sample_rate',
+            type: 'input',
+        });
 });
