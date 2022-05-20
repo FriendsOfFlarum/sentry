d4bc7d061d35f2ce4ad6fcbd2b0c10d2b87c7f62
Ian Morland <ian@morland.me>
2022-05-20 15:29:47 +0100

    ts

---
+++ js/src/admin/index.ts
@@ -0,0 +1,45 @@
+import app from 'flarum/admin/app';
+
+app.initializers.add('fof/sentry', () => {
+  app.extensionData
+    .for('fof-sentry')
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.dsn_label'),
+      setting: 'fof-sentry.dsn',
+      type: 'url',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.user_feedback_label'),
+      setting: 'fof-sentry.user_feedback',
+      type: 'boolean',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.monitor_performance_label'),
+      setting: 'fof-sentry.monitor_performance',
+      type: 'number',
+      min: 0,
+      max: 100,
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.javascript_label'),
+      setting: 'fof-sentry.javascript',
+      type: 'boolean',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.javascript_console_label'),
+      setting: 'fof-sentry.javascript.console',
+      type: 'boolean',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.javascript_trace_sample_rate'),
+      setting: 'fof-sentry.javascript.trace_sample_rate',
+      type: 'number',
+      min: 0,
+      max: 100,
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-sentry.admin.settings.send_user_emails_label'),
+      setting: 'fof-sentry.send_emails_with_sentry_reports',
+      type: 'boolean',
+    });
+});
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,3 +1,4 @@
+import app from 'flarum/forum/app';
 import * as Sentry from '@sentry/browser';
 import { CaptureConsole } from '@sentry/integrations';
 import { Integrations as TracingIntegrations } from '@sentry/tracing';
@@ -9,24 +10,24 @@ window.Sentry.TracingIntegrations = TracingIntegrations;
 // All Sentry initialisation happens in `src/Content/SentryJavaScript.php`
 
 window.Sentry.getUserData = (nameAttr = 'username') => {
-    /** @type {Sentry.User} */
-    let userData = {};
+  /** @type {Sentry.User} */
+  let userData = {};
 
-    // Depending on when the error occurs, `app` might not be defined
-    if (app) {
-        if (app.session && app.session.user && app.session.user.id() != 0) {
-            userData = {
-                ip_address: '{{auto}}',
-                id: app.session.user.id(),
-                email: app.session.user.email(),
-                [nameAttr]: app.session.user.username(),
-            };
-        } else if (app.data.session && app.data.session.userId != 0) {
-            userData = {
-                id: app.data.session.userId,
-            };
-        }
+  // Depending on when the error occurs, `app` might not be defined
+  if (app) {
+    if (app.session && app.session.user && app.session.user.id() != 0) {
+      userData = {
+        ip_address: '{{auto}}',
+        id: app.session.user.id(),
+        email: app.session.user.email(),
+        [nameAttr]: app.session.user.username(),
+      };
+    } else if (app.data.session && app.data.session.userId != 0) {
+      userData = {
+        id: app.data.session.userId,
+      };
     }
+  }
 
-    return userData;
+  return userData;
 };
