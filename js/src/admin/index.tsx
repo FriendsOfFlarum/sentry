9f0af31408776e0090fad06678eb7c1008952dea
IanM <ian@morland.me>
2025-12-16 21:39:01 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -2,5 +2,5 @@ import app from 'flarum/admin/app';
 import SentrySettingsPage from './components/SentrySettingsPage';
 
 app.initializers.add('fof/sentry', () => {
-  app.extensionData.for('fof-sentry').registerPage(SentrySettingsPage);
+  app.registry.for('fof-sentry').registerPage(SentrySettingsPage);
 });
